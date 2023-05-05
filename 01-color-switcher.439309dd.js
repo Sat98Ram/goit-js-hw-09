539850fdb3b91879ee84153b6263c034a774400e
Sat98Ram <108619661+Sat98Ram@users.noreply.github.com>
2023-05-05 14:12:42 +0000

    Deploying to gh-pages from @ Sat98Ram/goit-js-hw-09@c958ff090e8fb764e43db6a53827c893660ccae7 🚀

---
+++ 01-color-switcher.439309dd.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;const a=document.querySelector("body");t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,d=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.439309dd.js.map
