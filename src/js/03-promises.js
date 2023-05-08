e128458b1625b8b7084fa1a4fe0482b5ca6e4842
Iryna <irinkasatanovskaya@gmail.com>
2023-05-08 16:58:49 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,10 +6,35 @@ const delayStep = document.querySelector('[name="step"]');
 const selectedAmount = document.querySelector('[name="amount"]');
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let delay = Number(firstDelay.value);
+  const step = Number(delayStep.value);
+  const amount = selectedAmount.value;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(obj => {
+        Notiflix.Notify.success(`Fulfilled promise ${i} in ${obj.delay}ms`);
+      })
+      .catch(obj => {
+        Notiflix.Notify.failure(`Rejected promise ${i} in ${obj.delay}ms`);
+      });
+
+    delay += step;
+    console.log(delay);
+  }
+  event.currentTarget.reset();
+});
